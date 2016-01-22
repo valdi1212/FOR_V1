f24af7af6b464b08ace71ba02effc46d0064de5d
valdi1212 <valdi1212@gmail.com>
2016-01-22 21:46:40 +0000

    Adding more code to blackjack -- can't seem to change some variables which completely halts the progress of the program

--- BlackJack.py
+++ BlackJack.py
@@ -1,7 +1,146 @@
 import random
 
 __author__ = 'Valdimar Gunnarsson'
+cardMap = {
+    1: 'A',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: '10',
+    11: 'J',
+    12: 'Q',
+    13: 'K'
+}
+
+cardValues = {
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'J': 10,
+    'Q': 10,
+    'K': 10
+}
 
 
-class BlackJack:
-    points = 50000
+def keep_playing():
+    print("Do you wish to play another round?(y/n)")
+    while True:
+        choice = input()
+        if choice == 'y':
+            super.keepPlaying = True
+            break
+        elif choice == 'n':
+            super.keepPlaying = False
+            break
+        else:
+            print("Wrong input. Please try again")
+
+
+def show_points():
+    print("Your points: " + str(points))
+
+
+def show_cards(card_list):
+    card_string = ''
+    if len(card_list) > 1:
+        for i in range(0, len(card_list) - 1):
+            card_string += card_list[i] + ', '
+    card_string += card_list[len(card_list) - 1]
+    return card_string
+
+
+def add_total(card_list, total, is_player):
+    if (card_list[-1] != 'A') or (is_player is False):
+        total += cardValues[card_list[-1]]
+    else:
+        print("You have been dealt an ace.\nDo you wish to count its value as 11(y/n)")
+        while True:
+            choice = input()
+            if choice == 'y':
+                total += 11
+                break
+            elif choice == 'n':
+                total += 1
+                break
+            else:
+                print("Wrong input. Please try again")
+    return total
+
+
+def deal_cards(card_list):
+    rand = random.randint(1, 13)
+    return card_list.append(cardMap[rand])
+
+
+def start_round():
+    deal_cards(playerCards)
+    add_total(playerCards, playerTotal, True)
+    deal_cards(playerCards)
+    add_total(playerCards, playerTotal, True)
+    deal_cards(dealerCards)
+    add_total(dealerCards, dealerTotal, False)
+    deal_cards(dealerCards)
+    add_total(dealerCards, dealerTotal, False)
+    print("Dealers's upface card: " + dealerCards[0])
+    print("Your cards: " + show_cards(playerCards))
+
+
+def show_menu():
+    print("\nType 'hit' to receive another card.")
+    print("Type 'stand' to stop recieving cards.\n")
+    while True:
+        choice = input()
+        if choice == 'hit':
+            deal_cards(playerCards)
+            add_total(playerCards, playerTotal, True)
+            print("Your cards: " + show_cards(playerCards))
+            break
+        elif choice == 'stand':
+            print("You chose to stand")
+            # do something?
+            break
+        else:
+            print("Wrong input. Please try again.")
+
+
+keepPlaying = True
+points, bet = 50000, 0
+playerCards, dealerCards = [], []
+playerTotal, dealerTotal = 0, 0
+
+while keepPlaying:
+    bet = 0
+    show_points()
+    while bet == 0:
+        bet = int(input("How much do you want to bet?"))
+        if bet > points:
+            print("You can't bet more than you have.")
+            bet = 0
+        elif bet < 500:
+            print("Minimum bet is 500")
+            bet = 0
+    points -= bet
+    start_round()
+    while True:
+        show_menu()
+        print(playerTotal)
+        if playerTotal > 21:
+            print("You go bust and lose " + str(bet) + " points.")
+            break
+        elif dealerTotal > 21:
+            print("The dealer goes bust, causing you to win " + str(bet) + " points.")
+            bet *= 2
+            points += bet
+            break
+    keep_playing()
--- YahtzeeGame.py
+++ YahtzeeGame.py
@@ -4,48 +4,35 @@ __author__ = 'Valdimar Gunnarsson'
 
 
 def check_score(player_score):
-    """This function checks the score of the player and prints it in the console.
-    :param player_score: the player's score
-    """
     print("Your current score is: " + str(player_score))
 
 
 def roll(player_dice):
-    """This function rolls the dice for the player
-    :param player_dice: list of the player's dice
-    """
     for i in range(0, 5):
         player_dice[i] = random.randint(1, 6)
         print("Dice %d: %d" % ((i + 1), player_dice[i]))
 
 
 def reroll(dice, player_dice):
-    """ Rerolls the dice the player picks
-    :param dice: string for the dice
-    :param player_dice: values of player's dice
-    """
     for item in dice:
         player_dice[item - 1] = random.randint(1, 6)
 
+playerScore = 0
+playerDice = [None] * 5
+print("Welcome to yahtzee.")
+print("To see your score in between rounds, simply type 'score'")
+print("To reroll your dice type 'reroll'")
+print("To pick which dice you wish to reroll, and which to keep, type their numbers in a row.")
+print("E.g. '1,3,4' This would keep dice number 2 and 5, but reroll dice number 1, 3 and 4")
+print("If you wish to reroll all your dice type '0'")
+print("Starting game now.\n")
 
-class Yahtzee:
-    playerScore = 0
-    playerDice = [None] * 5
-    print("Welcome to yahtzee.")
-    print("To see your score in between rounds, simply type 'score'")
-    print("To reroll your dice type 'reroll'")
-    print("To pick which dice you wish to reroll, and which to keep, type their numbers in a row.")
-    print("E.g. '1,3,4' This would keep dice number 2 and 5, but reroll dice number 1, 3 and 4")
-    print("If you wish to reroll all your dice type '0'")
-    print("Starting game now.\n")
+print("Testing player_roll function:")
 
-    print("Testing player_roll function:")
+roll(playerDice)
 
-    roll(playerDice)
-
-    rerollString = raw_input("Reroll your dice: ")
-    rerollList = rerollString.split(',')
-    rerollList = [int(i) for i in rerollList]
-    reroll(rerollList, playerDice)
-    print(playerDice)
-
+rerollString = input("Reroll your dice: ")
+rerollList = rerollString.split(',')
+rerollList = [int(i) for i in rerollList]
+reroll(rerollList, playerDice)
+print(playerDice)
